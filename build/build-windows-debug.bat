647427fc0fe0b35f28d8c3899108c8d37aae12fc
Dmitry Schetnikovich <tschetnikovich@gmail.com>
2012-11-05 22:42:16 +0300

    build for windows script

--- build/build-windows-debug.bat
+++ build/build-windows-debug.bat
@@ -1,16 +1,32 @@
 @echo off 
 
 REM Store current directory in order to return to it after
-SET ORIGINAL_DIR=%CD%
+set ORIGINAL_DIR=%CD%
 
 REM Find project root folder
-SET PROJECT_ROOT=%CD%\..\
+set PROJECT_ROOT=%CD%\..\
+
+REM Create target folder
+mkdir %PROJECT_ROOT%target\debug
 
 REM Go to /target/debug folder
-cd %PROJECT_ROOT%/target/debug
+cd %PROJECT_ROOT%target\debug
 
 REM Run qmake
-qmake %PROJECT_ROOT%/src/play.pro -r -spec win32-msvc2010 "CONFIG+=debug" "CONFIG+=declarative_debug"
+qmake %PROJECT_ROOT%src\play.pro -r -spec win32-msvc2010 "CONFIG+=debug" "CONFIG+=declarative_debug"
+
+
+REM ---- Make ---------
+
+set VCDIR="%ProgramFiles%\Microsoft Visual Studio 10.0\VC"
+
+if not exist "%VCDIR%" (
+  set VCDIR=%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\VC
+)
+  
+call "%VCDIR%\bin\vcvars32.bat"
+
+nmake
 
 REM Return back to original folder
 cd %ORIGINAL_DIR%
