30f15bf3bc52d4bee14d1563f818cfcb9ef40bbe
Dmitry Schetnikovich <tschetnikovich@gmail.com>
2012-11-06 19:18:17 +0300

    windows build script updates

--- build/build-windows-debug.bat
+++ build/build-windows-debug.bat
@@ -1,32 +1,83 @@
 @echo off 
 
+REM -----------------------------------
+REM - Configuration
+REM -----------------------------------
+
+REM Visual C++ directory
+set VISUALC_DIR=%ProgramFiles%\Microsoft Visual Studio 10.0\VC
+
 REM Store current directory in order to return to it after
 set ORIGINAL_DIR=%CD%
 
 REM Find project root folder
 set PROJECT_ROOT=%CD%\..\
 
+echo Visual C folder: %VISUALC_DIR%
+
+REM -----------------------------------
+REM - Preparation
+REM -----------------------------------
+
 REM Create target folder
-mkdir %PROJECT_ROOT%target\debug
+set OUTPUT=%PROJECT_ROOT%target\debug
+if not exist %OUTPUT% mkdir %OUTPUT%
+if %ERRORLEVEL% neq 0 goto ERROR
+
+echo Output folder: %OUTPUT%
 
 REM Go to /target/debug folder
 cd %PROJECT_ROOT%target\debug
+if %ERRORLEVEL% neq 0 goto ERROR
+
+REM -----------------------------------
+REM - QMake
+REM -----------------------------------
 
 REM Run qmake
 qmake %PROJECT_ROOT%src\play.pro -r -spec win32-msvc2010 "CONFIG+=debug" "CONFIG+=declarative_debug"
+if %ERRORLEVEL% neq 0 goto ERROR
 
+REM -----------------------------------
+REM - Visual C nmake
+REM -----------------------------------
 
-REM ---- Make ---------
+echo VSINSTALLDIR is: %VSINSTALLDIR%
 
-set VCDIR="%ProgramFiles%\Microsoft Visual Studio 10.0\VC"
+IF "!%VSINSTALLDIR%!" == "!!" (
 
-if not exist "%VCDIR%" (
-  set VCDIR=%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\VC
-)
+  call "%VISUALC_DIR%\bin\vcvars32.bat"
+  if %ERRORLEVEL% neq 0 goto ERROR
   
-call "%VCDIR%\bin\vcvars32.bat"
+) else ( 
+echo Skipping VC environment setup
+)
 
 nmake
+if %ERRORLEVEL% neq 0 goto ERROR
+
+goto DONE
+
+REM -----------------------------------
+REM - Errors report
+REM -----------------------------------
+:ERROR
+
+echo Some error found.
+goto FINALLY
+
+REM -----------------------------------
+REM - Success report
+REM -----------------------------------
+:DONE
+
+echo Done without errors.
+goto FINALLY
+
+REM -----------------------------------
+REM - Block that is always executed
+REM -----------------------------------
+:FINALLY
 
 REM Return back to original folder
 cd %ORIGINAL_DIR%
